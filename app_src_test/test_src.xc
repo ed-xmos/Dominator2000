5ee1fe2981b34a43b78ca95e566b5df40c814157
Ed Clarke <ed@xmos.com>
2017-02-07 08:45:47 +0000

    More filter tweaking and timing (1383 cycles)

--- app_src_test/test_src.xc
+++ app_src_test/test_src.xc
@@ -12,12 +12,17 @@ int main(void){
 	src_init(&src_ctl);
 
 	for (int i=0; i<N_ITERATIONS; i++) {
+		timer t;
+		int t0, t1;
 		int sample_in;
 		sample_in = sine_44k[i];
 		//if (i == 0) sample_in = 0x7FFFFFFF;
 		//else sample_in = 0;
 		//printf("%d\n", sample_in);
+		t :> t0;
 		src_process(sample_in, &output[i * 8], &src_ctl);
+		t :> t1;
+		//printf("Time in ticks = %d\n", t1 - t0);
 	}
 
 	for (int i=0; i<N_ITERATIONS*8; i++) {
--- module_src/src_fir_generator.py
+++ module_src/src_fir_generator.py
@@ -25,9 +25,9 @@ def plot_response_passband(fs, w, h, title):
     plt.title(title)
 
 
-def make_filter(fs, transition_low, transition_high, numtaps, name):
+def make_filter(fs, transition_low, transition_high, numtaps, name, weight):
     interpolation_factor = 2
-    taps = signal.remez(numtaps, [0, transition_low, transition_high, 0.5*fs], [1, 0], weight=[.01, 1], Hz=fs)
+    taps = signal.remez(numtaps, [0, transition_low, transition_high, 0.5*fs], [1, 0], weight=weight, Hz=fs)
     w, h = signal.freqz(taps)
     plot_response(fs, w, h, "Low-pass Filter")
     #plot_response_passband(fs, w, h, "Low-pass Filter")
@@ -63,26 +63,29 @@ include_file = open("src/coeffs.h", "w")
 # Low-pass filter design parameters
 fs = 96000.0        # Sample rate, Hz
 numtaps = 32 * 2    # Size of the FIR filter.
-transition_low = fs / 2 * 0.45
-transition_high = fs / 2 * 0.55
+transition_low = fs / 2 * 0.41
+transition_high = fs / 2 * 0.51
 name = "stage_0_fir"
-content = make_filter(fs, transition_low, transition_high, numtaps, name)
+weight=[.2, .8]
+content = make_filter(fs, transition_low, transition_high, numtaps, name, weight)
 
 # Low-pass filter design parameters
 fs = 192000.0        # Sample rate, Hz
 numtaps = 16 * 2    # Size of the FIR filter.
-transition_low = fs / 4 * 0.45
-transition_high = fs / 4 * 0.55
+transition_low = fs / 4 * 0.41
+transition_high = fs / 4 * 0.51
 name = "stage_1_fir"
-content += make_filter(fs, transition_low, transition_high, numtaps, name)
+weight=[.2, 8]
+content += make_filter(fs, transition_low, transition_high, numtaps, name, weight)
 
 # Low-pass filter design parameters
 fs = 384000.0        # Sample rate, Hz
 numtaps = 16 * 2    # Size of the FIR filter.
-transition_low = fs / 8 * 0.45
-transition_high = fs / 8 * 0.55
+transition_low = fs / 4 * 0.41
+transition_high = fs / 4 * 0.51
 name = "stage_2_fir"
-content += make_filter(fs, transition_low, transition_high, numtaps, name)
+weight=[.4, .6]
+content += make_filter(fs, transition_low, transition_high, numtaps, name, weight)
 
 include_file.write(content)
 include_file.close()
