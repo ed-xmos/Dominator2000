109a43b44da4b37183fdd9436bb8975f7329b0a0
Ed Clarke <ed@xmos.com>
2017-02-07 21:25:16 +0000

    FIxed upsampler

--- module_src/src/us8_src.c
+++ module_src/src/us8_src.c
@@ -4,7 +4,7 @@
 #include "src_fir_inner_loop_asm.h"
 #include "coeffs.h"
 
-int stage_0_delay[STAGE_0_FIR_LENGTH] = {0};
+int stage_0_delay[STAGE_0_FIR_LENGTH] = {0}; //Double what it needs to be for circular buffer simulation
 int stage_1_delay[STAGE_1_FIR_LENGTH] = {0};
 int stage_2_delay[STAGE_2_FIR_LENGTH] = {0};
 
@@ -12,7 +12,7 @@ int stage_2_delay[STAGE_2_FIR_LENGTH] = {0};
 void src_init(src_ctrl_t *src_ctrl)
 {
 
-    //Setup delay line
+    //Setup delay lines
     src_ctrl->delay_base[0] = stage_0_delay;
     memset(stage_0_delay, 0, sizeof(stage_0_delay));
     src_ctrl->delay_len[0] = STAGE_0_FIR_LENGTH / 2;
@@ -34,7 +34,7 @@ void src_init(src_ctrl_t *src_ctrl)
     src_ctrl->inner_loops[1] = STAGE_1_FIR_LENGTH / 2;
     src_ctrl->inner_loops[2] = STAGE_2_FIR_LENGTH / 2;
 
-    src_ctrl->num_coeffs[0] = STAGE_0_FIR_LENGTH / 2;
+    src_ctrl->num_coeffs[0] = STAGE_0_FIR_LENGTH / 2; //per phase
     src_ctrl->num_coeffs[1] = STAGE_1_FIR_LENGTH / 2;
     src_ctrl->num_coeffs[2] = STAGE_2_FIR_LENGTH / 2;
 
@@ -54,36 +54,37 @@ int result; //Intermediate result
 void src_process(int in_samp, int out_buff[], src_ctrl_t *src_ctrl){
     //First stage steep filter up by 2
     int stage_0_out[2];
+
+    //Push sample in circular buffer
+    //printhexln(src_ctrl); 
+    int * delay_base = src_ctrl->delay_base[0] + src_ctrl->delay_idx[0]; 
+
+    *delay_base = in_samp;
+    *(delay_base + src_ctrl->delay_len[0]) = in_samp;
+
     for (int i=0; i<2; i++) {
-        //Push sample in circular buffer
-        //printhexln(src_ctrl); 
-        int * delay_base = src_ctrl->delay_base[0] + src_ctrl->delay_idx[0]; 
-
-        *delay_base = in_samp;
-        *(delay_base + src_ctrl->delay_len[0]) = in_samp;
-
         //Do the FIR
         //printintln(insamp);
+        //printintln(src_ctrl->inner_loops[0]);
         if ((unsigned)delay_base & 0b0100) {
             src_fir_inner_loop_asm_odd(
                  src_ctrl->delay_base[0] + src_ctrl->delay_idx[0]
                 ,src_ctrl->coeffs[0] + (src_ctrl->phase[0] * src_ctrl->num_coeffs[0]) //Polyphase bit
                 ,&stage_0_out[i]
-                ,src_ctrl->inner_loops[0] >> 1);
+                ,src_ctrl->inner_loops[0]);
         }
         else {
             src_fir_inner_loop_asm(
                  src_ctrl->delay_base[0] + src_ctrl->delay_idx[0]
                 ,src_ctrl->coeffs[0] + (src_ctrl->phase[0] * src_ctrl->num_coeffs[0]) //Polyphase bit
                 ,&stage_0_out[i]
-                ,src_ctrl->inner_loops[0] >> 1);
+                ,src_ctrl->inner_loops[0]);
         }
         //printintln(stage_0_out[i]);
         src_ctrl->phase[0] ^= 1;   //Swap phase of polyphase filter
-        
-        src_ctrl->delay_idx[0] += 1; 
-        if (src_ctrl->delay_idx[0] == src_ctrl->delay_len[0]) src_ctrl->delay_idx[0] = 0;
     }
+    src_ctrl->delay_idx[0] += 1; 
+    if (src_ctrl->delay_idx[0] == src_ctrl->delay_len[0]) src_ctrl->delay_idx[0] = 0;
 
     //Second stage relaxed filter up by 2
     int stage_1_out[4];
@@ -93,27 +94,29 @@ void src_process(int in_samp, int out_buff[], src_ctrl_t *src_ctrl){
         *delay_base = stage_0_out[i >> 1];
         *(delay_base + src_ctrl->delay_len[1]) = stage_0_out[i >> 1];
 
-        //printintln(*delay_base);
+        //printintln(src_ctrl->inner_loops[1]);
         //Do the FIR
         if ((unsigned)delay_base & 0b0100) {
             src_fir_inner_loop_asm_odd(
              delay_base
             ,src_ctrl->coeffs[1] + (src_ctrl->phase[1] * src_ctrl->num_coeffs[1]) //Polyphase bit
             ,&stage_1_out[i]
-            ,src_ctrl->inner_loops[1] >> 1);}
+            ,src_ctrl->inner_loops[1]);
+        }
         else {
             src_fir_inner_loop_asm(
              delay_base
             ,src_ctrl->coeffs[1] + (src_ctrl->phase[1] * src_ctrl->num_coeffs[1]) //Polyphase bit
             ,&stage_1_out[i]
-            ,src_ctrl->inner_loops[1] >> 1);
+            ,src_ctrl->inner_loops[1]);
         }
         //printintln(stage_1_out[i]);
 
         src_ctrl->phase[1] ^= 1;  //Swap phase of polyphase filter
-
-        src_ctrl->delay_idx[1] += 1;
-        if (src_ctrl->delay_idx[1] == src_ctrl->delay_len[1]) src_ctrl->delay_idx[1] = 0;
+        if (i & 1) {
+            src_ctrl->delay_idx[1] += 1;
+            if (src_ctrl->delay_idx[1] == src_ctrl->delay_len[1]) src_ctrl->delay_idx[1] = 0;
+        }
     }
 
     //Third stage relaxed filter up by 2
@@ -124,26 +127,28 @@ void src_process(int in_samp, int out_buff[], src_ctrl_t *src_ctrl){
         *delay_base = stage_1_out[i >> 1];
         *(delay_base + src_ctrl->delay_len[2]) = stage_1_out[i >> 1];
 
+        //printintln(src_ctrl->inner_loops[2]);
         //Do the FIR
         if ((unsigned)delay_base & 0b0100) {
             src_fir_inner_loop_asm_odd(
                  src_ctrl->delay_base[2] + src_ctrl->delay_idx[2]
                 ,src_ctrl->coeffs[2] + (src_ctrl->phase[2] * src_ctrl->num_coeffs[2]) //Polyphase bit
                 ,&out_buff[i]
-                ,src_ctrl->inner_loops[2] >> 1);
+                ,src_ctrl->inner_loops[2]);
         }
         else {
             src_fir_inner_loop_asm(
                  src_ctrl->delay_base[2] + src_ctrl->delay_idx[2]
                 ,src_ctrl->coeffs[2] + (src_ctrl->phase[2] * src_ctrl->num_coeffs[2]) //Polyphase bit
                 ,&out_buff[i]
-                ,src_ctrl->inner_loops[2] >> 1);
+                ,src_ctrl->inner_loops[2]);
         }
         //printintln(out_buff[i]);
 
-        src_ctrl->phase[2] ^= 1;  //Swap phase of polyphase filte
-        src_ctrl->delay_idx[2] += 1;
-
-        if (src_ctrl->delay_idx[2] == src_ctrl->delay_len[2]) src_ctrl->delay_idx[2] = 0;
+        src_ctrl->phase[2] ^= 1;  //Swap phase of polyphase filter
+        if (i & 1) {
+            src_ctrl->delay_idx[2] += 1;
+            if (src_ctrl->delay_idx[2] == src_ctrl->delay_len[2]) src_ctrl->delay_idx[2] = 0;
+        }
     }
 }
