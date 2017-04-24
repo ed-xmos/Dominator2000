edc798687f69ccf2f304387d3c5c313024e39897
Ed Clarke <ed@xmos.com>
2017-04-24 09:47:19 +0100

    FIx flash script in test prog

--- app_dom_test/flash_with_filesystem.sh
+++ app_dom_test/flash_with_filesystem.sh
@@ -1,2 +1,2 @@
 pushd ../mp3media/ && sh create_flash_image.sh && popd
-xflash --boot-partition-size 131072 --data ../mp3media/mp3_flash_image.bin bin/app_dominator2000.xe
+xflash --boot-partition-size 131072 --data ../mp3media/mp3_flash_image.bin bin/app_dom_test.xe
